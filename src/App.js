407d6c72df7240e7dd4f93a2d4d5052dec3336fd
Phạm Minh Tiến <phaminhtien2424@gmail.com>
2021-01-04 11:34:49 +0700

    (feat): cth

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class='box'>
+      <ul class='minbox'>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <ol class='maxbox'>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
